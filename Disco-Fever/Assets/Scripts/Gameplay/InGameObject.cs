d083e2d97bc643f8999636d80de98eee5886a807
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2017-03-26 11:22:20 +0200

    Initial commit.

---
+++ Disco-Fever/Assets/Scripts/Gameplay/InGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameObject : MonoBehaviour
+{
+    protected LevelManager LEVEL_MANAGER;
+
+    void Start()
+    {
+        LEVEL_MANAGER = GameObject.FindObjectOfType<LevelManager>();
+    }
+}
